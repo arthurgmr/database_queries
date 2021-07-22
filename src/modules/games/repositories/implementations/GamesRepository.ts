637abeac0e49dd2376224411d1a88ddf981934b5
Arthur Machado <arthur.gmr@hotmail.com>
2021-07-21 22:56:25 -0300

    queries-games

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,29 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
+      .createQueryBuilder("games")
+      .where("games.title ilike :param", {param: '%' + param + '%' })
+      .getMany()
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      SELECT COUNT(id) FROM games
+    `); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
+      .createQueryBuilder("games")
+      .leftJoinAndSelect("games.users", "users")
+      .select([
+        "users.first_name AS first_name",
+        "users.last_name AS last_name",
+        "users.email AS email"
+      ])
+      .where("games.id = :id", { id })
+      .getRawMany()
       // Complete usando query builder
   }
 }
